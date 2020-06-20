d6363dec2bda74cae58ce34404a285a539d94fca
Jason Wong <jwong853@gmail.com>
2020-06-20 15:19:20 -0500

    saturday

--- src/jw_total_df.py
+++ src/jw_total_df.py
@@ -77,4 +77,71 @@ def create_dataframe():
     
     df = merge_dataframes(ps_df[ps_columns], b_df[b_columns], p_df[p_columns], 'inner', ['Major', 'Minor'])
     
+    df = df[df['SalePrice'] > 0]
+    
+    has_porch = [1 if ((op > 0) | (ep > 0)) else 0 for op, ep in zip(df['SqFtOpenPorch'], df['SqFtEnclosedPorch'])]
+    df['has_porch'] = has_porch
+    
     return df
+
+def dummify(df, column_names):
+    dataframes = []
+    copy_df = df.copy()
+    for column in column_names:
+        new_df = pd.get_dummies(df[column], drop_first=True)
+        new_df.columns = [column + '_' + str(name) for name in new_df]
+        dataframes.append(new_df)
+        copy_df.drop(column, axis = 1, inplace = True)
+    new_df = pd.concat(dataframes, axis=1)
+    return pd.concat([copy_df, new_df], axis = 1)
+
+def pow_transformer(df , column_names):
+    dataframe = []
+    copy_df = df.copy()
+    for column in column_names:
+        new_df = power_transform(np.array(df[column]).reshape(-1,1))
+        new_df.columns = [column + '_' + str(name) for name in new_df]
+        dataframes.append(new_df)
+        copy_df.drop(column, axis=1, inplace=True)
+    new_df = pd.concat(dataframes, axis=1)
+    return pd.concat([copy_df, new_df], axis=1)
+
+import statsmodels.formula.api as smf
+
+def forward_selected(data, response):
+    """Linear model designed by forward selection.
+
+    Parameters:
+    -----------
+    data : pandas DataFrame with all possible predictors and response
+
+    response: string, name of response column in data
+
+    Returns:
+    --------
+    model: an "optimal" fitted statsmodels linear model
+           with an intercept
+           selected by forward selection
+           evaluated by adjusted R-squared
+    """
+    remaining = set(data.columns)
+    remaining.remove(response)
+    selected = []
+    current_score, best_new_score = 0.0, 0.0
+    while remaining and current_score == best_new_score:
+        scores_with_candidates = []
+        for candidate in remaining:
+            formula = "{} ~ {} + 1".format(response,
+                                           ' + '.join(selected + [candidate]))
+            score = smf.ols(formula, data).fit().rsquared_adj
+            scores_with_candidates.append((score, candidate))
+        scores_with_candidates.sort()
+        best_new_score, best_candidate = scores_with_candidates.pop()
+        if current_score < best_new_score:
+            remaining.remove(best_candidate)
+            selected.append(best_candidate)
+            current_score = best_new_score
+    formula = "{} ~ {} + 1".format(response,
+                                   ' + '.join(selected))
+    model = smf.ols(formula, data).fit()
+    return model
